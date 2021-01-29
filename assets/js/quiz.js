dc5c5b4e43c3660e01a2c219ea04eeaa3af3d056
OluwafemiAwosanya <oluwafemiawosanya@hotmail.com>
2021-01-29 19:37:32 +0000

    added showResults and slides functions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,11 +2,11 @@
 //Functions
 function setQuiz(){
     //variable to store the HTML output
-    var output = [];
+    const output = [];
     //for each question
     quizQuestions.forEach(currentQuestion, questionNumber => {
             //variable to store list of possible answers
-            var answers = [];
+            const answers = [];
             //and for each available answer
             for(letter in currentQuestion.answers){
                 //add an HTML radio button
@@ -30,13 +30,67 @@ function setQuiz(){
     quizContainer.innerHTML = output.join('');
 }
 
-function showResults(){}
+function showResults(){
+    //answer containers
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+    // track user's answers
+    let numCorrect = 0;
+    //each question
+    quizQuestions.forEach(
+        (currentQuestion, questionNumber) =>{
+            //find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            //if answer is correct
+            if(userAnswer === currentQuestion.correctAnswer){
+                numCorrect++;
+                //color the answers green
+                answerContainers[questionNumber].style.color='green';
+            }
+            //if answer is wrong
+            else{
+                //color the answers red
+                answerContainers[questionNumber].style.color='red';
+            }
+        }
+    );
+    resultsContainer.innerHTML = `${numCorrect} out of ${quizQuestions.length}`;
+}
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+        previousButton.style.display = 'none';
+    }
+    else{
+        previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1) {
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+    }
+    else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+    }
+}
+
+function showNextSlide (){
+    showSlide(currentSlide + 1);
+}
+function showPreviousSlide (){
+    showSlide(currentSlide - 1);
+}
 
 //Variables
-let quizContainer = document.getElementById('quiz');
-let resultsContainer = document.getElementById('results');
-let submitButton = document.getElementById('submit');
-let quizQuestions = [
+const startButton = document.getElementById('start');
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const quizQuestions = [
     {
         question: "The following are continents of the world except?",
         answers: {
@@ -132,5 +186,19 @@ let quizQuestions = [
 //Display quiz
 setQuiz();
 
-//Show results on submit
+//Pagination
+const previousButton = document.getElementById("previous");
+const nextButton = documment.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+//Event Listeners
+startButton.addEventListener('click', quizQuestions);
+
+showSlide(currentSlide);
+
 submitButton.addEventListener('click', showResults);
+
+previousButton.addEventListener('click', showPreviousSlide);
+
+nextButton.addEventListener('click', showNextSlide);
